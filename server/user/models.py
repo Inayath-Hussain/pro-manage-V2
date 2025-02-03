c58b29fbd1a86c8e5ea574ed5b2c606d2c908b52
Inayath <inayathh2@gmail.com>
2025-02-03 21:55:24 +0530

    feat: custom user model
    added custom user model and its related manager in django server.

---
+++ server/user/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
+
+from .managers import CustomUserManager 
+
+# Create your models here.
+
+# create user model and override default user
+class User(AbstractBaseUser):
+    name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'email']
+
+    objects = CustomUserManager()
+
+
+    def __str__(self):
+        return self.email
